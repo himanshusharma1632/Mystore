c5fce6067b0a62e0791b58c3387a8c477f9034f5
himanshusharma1632 <himsharma1632000@gmail.com>
2024-12-20 17:07:37 +0530

    Merge "Inventory" to "Main"

--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c7140ff38e8bedab32be7049bc21bcfc5584f7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
